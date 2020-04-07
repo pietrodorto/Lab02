77945e5a9fec44c26ab912bdc55eb370cb0f75d8
pietrodorto <s249615@studenti.polito.it>
2020-04-07 12:42:02 +0200

    aggiornato controller

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,40 +1,48 @@
 package it.polito.tdp.alien;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
     @FXML
-    private ResourceBundle resources;
+    private TextField txtWord;
 
     @FXML
-    private URL location;
-    
-    @FXML
     private Button btnTranslate;
-    
+
     @FXML
-    private Button btnReset;
+    private TextArea txtDictionary;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	txtWord.clear();
+    	txtDictionary.clear();
+
+    }
 
     @FXML
     void doTranslate(ActionEvent event) {
     	
-    
-    }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
-    }
-    
-    
-    @FXML
-    void initialize() {
+    	
+    	String word = txtWord.getText();
+    	
+    	if(word != null || word.length()!= 0)
+    	StringTokenizer st = new StringTokenizer(word," ");
+    	
+    	String alien = st.nextToken();
+    	
+    	if()
+    	
 
     }
+
 }
