ed16387179fd52d095410d0a9c61435015968e7c
pietrodorto <s249615@studenti.polito.it>
2020-04-07 18:18:38 +0200

    do translate

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/AlienDictionary.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/AlienDictionary.java
@@ -2,33 +2,31 @@ package it.polito.tdp.alien;
 
 import java.util.*;
 
-public class AlienDictionaty {
-	
+public class AlienDictionary {
+
 	List<Word> dictionary = new ArrayList<>();
-	
-	public AlienDictionaty(){
+
+	public AlienDictionary() {
 		super();
 	}
-	
+
 	public void addWord(String alien, String trans) {
-		
+
 		Word w = new Word(alien, trans);
-		if(dictionary.contains(w)) {
+		if (dictionary.contains(w)) {
 			dictionary.get(dictionary.indexOf(w)).setTranslation(trans);
-			}
+		}
 		dictionary.add(w);
 	}
-	
+
 	public String translateWord(String alienWord) {
-		
+
 		Word datradurre = new Word(alienWord);
-		if(dictionary.contains(datradurre)) {
+		if (dictionary.contains(datradurre)) {
 			return dictionary.get(dictionary.indexOf(datradurre)).getTranslation();
 		}
 		return null;
-		
-			
+
 	}
-	
-	
+
 }
--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private AlienDictionary dictionary = new AlienDictionary();
 
     @FXML
     private TextField txtWord;
@@ -32,8 +34,8 @@ public class FXMLController {
     @FXML
     void doTranslate(ActionEvent event) {
     	
-    	txtWord.clear();
-    	String word = txtWord.getText();
+    	
+    	String word = txtWord.getText().toLowerCase();
     	
     	StringTokenizer st = new StringTokenizer(word," ");
     	
@@ -43,16 +45,24 @@ public class FXMLController {
     	}
     	
     	String alien = st.nextToken();
-    	if(!alien.matches("[[A-Za-z]]*"))
+    	
     	
     	if(st.hasMoreTokens()) {
     		
-    		String trans = st.nextToken();
+    		String trans = st.nextToken().toLowerCase();
     		
+    		if(!alien.matches("[a-zA-Z]*") || !trans.matches("[a-zA-Z]*")) {
+    			txtDictionary.setText("Inserici una o più parole");
+        		return;
+    		}
     		
-    		
-    		
+    		dictionary.addWord(alien, trans);
+    		txtDictionary.setText("hai aggiunto la parola :"+alien+" / "+trans);
+    				
     	}
+    	
+    	String trad = dictionary.translateWord(alien);
+    	txtDictionary.setText(trad.toString());
     	
     
     	
